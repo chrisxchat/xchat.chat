5d0cea8f09a285e9c96f40480fe727be87819481
Oleksandr Hubachov <alex369.xsa@yahoo.com>
2023-04-09 19:00:43 +0300

    Added ability to send SMS messages to particular phone number

--- ChatGptEchoFunction/src/main/java/chat/xchat/ChatGptEchoFunction.java
+++ ChatGptEchoFunction/src/main/java/chat/xchat/ChatGptEchoFunction.java
@@ -6,6 +6,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.PublishRequest;
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -18,11 +20,15 @@ import java.util.Map;
 
 /**
  * Handler for requests to Lambda function.
+ * TODO:
+ * 1. Store API keys securely
+ * 2. Attach database to store message history
+ * 3. Trigger this lambda by SNS when AWS Pinpoint phone number registered
  */
 public class ChatGptEchoFunction implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	private HttpClient client = HttpClient.newHttpClient();
-	private Gson gson = new Gson();
+	private final HttpClient client = HttpClient.newHttpClient();
+	private final Gson gson = new Gson();
 
 	public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 		Map<String, String> headers = new HashMap<>();
@@ -32,26 +38,35 @@ public class ChatGptEchoFunction implements RequestHandler<APIGatewayProxyReques
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
 				.withHeaders(headers);
 		try {
-
-			ChatGptRequest requestBody = new ChatGptRequest();
-			requestBody.setPrompt("How to play golf?");
-			HttpRequest request = HttpRequest.newBuilder()
-					.uri(URI.create("https://api.openai.com/v1/completions"))
-					.header("Content-Type", "application/json")
-					.header("Authorization", "Bearer ***")
-					.header("OpenAI-Organization", "****")
-					.POST(HttpRequest.BodyPublishers.ofString(gson.toJson(requestBody)))
-					.build();
-
-			HttpResponse<String> res = client.send(request, HttpResponse.BodyHandlers.ofString());
+			String text = askChatGpt();
+			sendSms("", text);
 			return response
 					.withStatusCode(200)
-					.withBody(toChatGptResponse(res.body()).getChoices().get(0).getText());
+					.withBody(text);
 		} catch (InterruptedException | IOException e) {
 			return response
 					.withBody("{}")
 					.withStatusCode(500);
 		}
+	}
+
+	private void sendSms(String phoneNumber, String text) {
+		AmazonSNSClientBuilder.standard().build().publish(new PublishRequest().withPhoneNumber(phoneNumber).withMessage(text));
+	}
+
+	private String askChatGpt() throws IOException, InterruptedException {
+		ChatGptRequest requestBody = new ChatGptRequest();
+		requestBody.setPrompt("How to play poker?");
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("https://api.openai.com/v1/completions"))
+				.header("Content-Type", "application/json")
+				.header("Authorization", "Bearer ")
+				.header("OpenAI-Organization", "")
+				.POST(HttpRequest.BodyPublishers.ofString(gson.toJson(requestBody)))
+				.build();
+
+		HttpResponse<String> res = client.send(request, HttpResponse.BodyHandlers.ofString());
+		return toChatGptResponse(res.body()).getChoices().get(0).getText();
 	}
 
 	private ChatGptResponse toChatGptResponse(String json) {
