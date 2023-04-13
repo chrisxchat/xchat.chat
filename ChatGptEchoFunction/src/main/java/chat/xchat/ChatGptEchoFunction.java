ba0ca1ede2c8b1308772a5fd8a5ee50e2da1bf7e
Oleksandr Hubachov <alex369.xsa@yahoo.com>
2023-04-13 18:05:59 +0300

    Version for Telegram

--- ChatGptEchoFunction/src/main/java/chat/xchat/ChatGptEchoFunction.java
+++ ChatGptEchoFunction/src/main/java/chat/xchat/ChatGptEchoFunction.java
@@ -11,15 +11,20 @@ import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.util.CollectionUtils;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -35,6 +40,10 @@ public class ChatGptEchoFunction implements RequestHandler<APIGatewayProxyReques
 	private final HttpClient client = HttpClient.newHttpClient();
 	private final Gson gson = new Gson();
 
+	private final SecretsManagerClient secretsManagerClient = SecretsManagerClient.builder()
+			.region(Region.of("us-east-1"))
+			.build();
+
 	public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 		Map<String, String> headers = new HashMap<>();
 		headers.put("Content-Type", "application/json");
@@ -42,9 +51,16 @@ public class ChatGptEchoFunction implements RequestHandler<APIGatewayProxyReques
 
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
 				.withHeaders(headers);
+		String tgMessage = gson.fromJson(input.getBody(), JsonObject.class).getAsJsonObject("message").get("text").getAsString();
+		if (!tgMessage.contains("@xchat_dev_bot")) {
+			return response
+					.withStatusCode(200);
+		}
 		try {
-			String text = askChatGpt().strip();
-			sendSms("+380933506675", text);
+			String message = tgMessage.replaceAll("@xchat_dev_bot", "");
+			String text = askChatGpt(message);
+//			sendSms("+380933506675", text);
+			sendTelegramMessage(text);
 			return response
 					.withStatusCode(200)
 					.withBody(text);
@@ -55,13 +71,25 @@ public class ChatGptEchoFunction implements RequestHandler<APIGatewayProxyReques
 		}
 	}
 
+	private void sendTelegramMessage(String text) throws IOException {
+		String textEncoded = URLEncoder.encode(text, StandardCharsets.UTF_8);;
+		String tgToken = getTelegramBotToken();
+		String chatId = "-944418211";
+		String urlString = "https://api.telegram.org/bot" + tgToken + "/sendMessage?chat_id=" + chatId + "&text=" + textEncoded;
+		URL url = new URL(urlString);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		con.getResponseCode();
+	}
+
 	private void sendSms(String phoneNumber, String text) {
 		AmazonSNSClientBuilder.standard().build().publish(new PublishRequest().withPhoneNumber(phoneNumber).withMessage(text));
 	}
 
-	private String askChatGpt() throws IOException, InterruptedException {
+	private String askChatGpt(String message) throws IOException, InterruptedException {
 		ChatGptRequest requestBody = new ChatGptRequest();
-		requestBody.setPrompt("How to play squid game?");
+		requestBody.setPrompt(message);
+		requestBody.setMax_tokens(100);
 		HttpRequest request = HttpRequest.newBuilder()
 				.uri(URI.create("https://api.openai.com/v1/completions"))
 				.header("Content-Type", "application/json")
@@ -75,6 +103,7 @@ public class ChatGptEchoFunction implements RequestHandler<APIGatewayProxyReques
 				.map(ChatGptResponse::getChoices)
 				.map(choices -> CollectionUtils.isNullOrEmpty(choices) ? null : choices.get(0))
 				.map(ChoiceDto::getText)
+				.map(String::strip)
 				.orElse(res.body());
 	}
 
@@ -85,13 +114,18 @@ public class ChatGptEchoFunction implements RequestHandler<APIGatewayProxyReques
 		return gson.fromJson(json, ChatGptResponse.class);
 	}
 
-	// TODO: check performance
 	private String getChatGptApiKey() {
-		SecretsManagerClient client = SecretsManagerClient.builder()
-				.region(Region.of("us-east-1"))
-				.build();
-		return client.getSecretValue(GetSecretValueRequest.builder()
-				.secretId("ChatGptApiKey")
+		return getSecret("ChatGptApiKey");
+	}
+
+	private String getTelegramBotToken() {
+		return getSecret("TelegramBotTokenDev");
+	}
+
+	// TODO: check performance
+	private String getSecret(String name) {
+		return secretsManagerClient.getSecretValue(GetSecretValueRequest.builder()
+				.secretId(name)
 				.build()).secretString();
 	}
 
---
+++ ChatGptEchoFunction/src/main/java/chat/xchat/dto/TelegramRequest.java
@@ -0,0 +1,5 @@
+package chat.xchat.dto;
+
+
+public class TelegramRequest {
+}
